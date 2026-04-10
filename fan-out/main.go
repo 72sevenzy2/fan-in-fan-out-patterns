dc69122fe361272a46fbdd06cbf2f70e802a7f5c
72 <72sevenzy2@gmail.com>
2026-04-10 09:18:46 +0500

    example fan out

--- fan-out/main.go
+++ fan-out/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(wg *sync.WaitGroup, id int, tasks <-chan int, result chan<- int) {
+	defer wg.Done()
+	for i := range tasks {
+		fmt.Println("work received with", id)
+		result <- i
+	}
+}
 
 func main() {
-	fmt.Println("test")
+	tasks := make(chan int)
+	res := make(chan int)
+
+	var wg sync.WaitGroup
+
+	for i := range 3 {
+		wg.Add(1)
+		go worker(&wg, i, tasks, res)
+	}
+
+	go func() {
+		for i := range 8 {
+			tasks <- i
+		}
+		close(tasks)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	for range res {
+		fmt.Println(<-res)
+	}
 }
