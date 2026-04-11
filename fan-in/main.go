8a47ba4865847697e92293c2a8c1682b9e99892c
72 <72sevenzy2@gmail.com>
2026-04-11 14:34:57 +0500

    refactor: replaced time sleeps with waitGroups.

--- fan-in/main.go
+++ fan-in/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func worker(id int, ch chan<- string) {
 	for range 3 {
 		ch <- fmt.Sprintf("received work with id %d", id)
-		time.Sleep(time.Millisecond * 500)
 	}
 	close(ch)
 }
 
-func fanin(channels ...<-chan string) <-chan string {
+func fanin(wg *sync.WaitGroup, channels ...<-chan string) <-chan string {
 	out := make(chan string)
+	wg.Add(len(channels))
 	for _, value := range channels {
 		go func (val <-chan string)  {
+			defer wg.Done()
 			for i := range val {
 				out <- i
 			}
@@ -24,7 +25,7 @@ func fanin(channels ...<-chan string) <-chan string {
 	}
 
 	go func ()  {
-		time.Sleep(2 * time.Second)
+		wg.Wait()
 		close(out)
 	}()
 
@@ -35,10 +36,12 @@ func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
+	var wg sync.WaitGroup;
+
 	go worker(1, ch1)
 	go worker(2, ch2)
 
-	merged := fanin(ch1, ch2)
+	merged := fanin(&wg, ch1, ch2)
 
 	for val := range merged {
 		fmt.Println(val)
