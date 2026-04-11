0c1f12587f11c3217561ba1f3955fae5a468ce1d
72 <72sevenzy2@gmail.com>
2026-04-11 13:54:28 +0500

    add fan-in example

--- fan-in/main.go
+++ fan-in/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, ch chan<- string) {
+	for i := 0; i < 3; i++ {
+		ch <- fmt.Sprintf("Worker %d: job %d", id, i)
+		time.Sleep(time.Millisecond * 500)
+	}
+	close(ch)
+}
+
+func fanIn(channels ...<-chan string) <-chan string {
+	out := make(chan string)
+	for _, ch := range channels {
+		go func(c <-chan string) {
+			for val := range c {
+				out <- val
+			}
+		}(ch)
+	}
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		close(out)
+	}()
+
+	return out
+}
 
 func main() {
-	fmt.Println("test")
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go worker(1, ch1)
+	go worker(2, ch2)
+
+	merged := fanIn(ch1, ch2)
+
+	for msg := range merged {
+		fmt.Println(msg)
+	}
 }
